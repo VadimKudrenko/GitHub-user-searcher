34188af8818ebdae6aae04304a69fb090c6d24e5
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2023-09-26 19:56:35 +0300

    add description of application on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import { useLocalStorage } from './hooks/useLocalStorage'
+
 import './App.css';
+import Home from './components/Home/Home'
+import UserInfo from './components/UserInfo/UserInfo'
+
+// interface IUserInfo {
+//   login: string;
+// }
 
 function App() {
+
+  const [userInfo, setUserInfo] = useLocalStorage([], 'userInfo')
+  const [userRepo, setUserRepo] = useLocalStorage([], 'userRepo')
+
+  function getUser (user: any) {
+    setUserInfo(user)
+  }
+
+  function getRepo(repo: any) {
+    setUserRepo(repo)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <h5 style={{color: 'rgb(98 98 98)', margin: '20px 20px', textAlign: 'left', fontSize: '15px', width: '550px'}}>
+        This application was made as a test task. <br/>
+        Main point was implement application which search users by name, <br/>
+        ability to select a user and find out more information about him. <br/>
+        Search field was made according to principle lazy loading, <br/>
+        this was made to send fewer requests on server.
+         <br/>
+        </h5>
+        <h1>Github Searcher</h1>
+        <Routes>
+          <Route path='/' element={<Home getUser={getUser} getRepo={getRepo} />}/>
+          <Route path='/user-info' element={<UserInfo user={userInfo} repo={userRepo}/>}/>
+        </Routes>
     </div>
   );
 }
